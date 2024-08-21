94d83ebf6ee0e85554d0cff69b58e6819c45cbb0
joaot1 <joaotombinibortolotti@Gmail.com>
2024-08-21 20:46:54 -0300

    Criação do projeto em java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "Thiago";
+
+        /*
+        > - Maior
+        >= - Maior Igual
+        < - Menor
+        <= - Menor igual
+        == - É igual
+        != - é Diferente
+        equal compara textos.
+         */
+
+        if (idadePessoa >= 21) {
+            System.out.println("é maior que 21");
+        }else {
+            System.out.println("Não é maior que 21");
+        }
+
     }
 }
